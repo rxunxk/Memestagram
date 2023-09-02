a2b23e29dae00fb052c43fab7e6d7c9969a6bb77
Raunak <pandeyraunak64@gmai.com>
2023-09-02 15:57:19 +0530

    fixed responsive behaviour

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -26,7 +26,7 @@ const Profile = () => {
   return (
     <div className="h-screen w-screen flex flex-col items-center bg-black text-white">
       <TopNavBar />
-      <div className="w-[600px] max-w-full mt-[75px] px-4">
+      <div className="w-[600px] max-w-full mt-[75px] px-1">
         {Object.keys(user) ? (
           <div className="flex gap-4 mt-4">
             <div className="shrink-0">
@@ -38,13 +38,13 @@ const Profile = () => {
               />
             </div>
             <div className="flex flex-col gap-2 w-full">
-              <div className="flex items-center justify-center gap-4">
+              <div className="flex items-center justify-center gap-4 max-[300px]:flex-col">
                 <div className="text-[1.3rem]">{`${user.fName} ${user.lName}`}</div>
-                <Button className="bg-[#0e92f1] h-9 hover:bg-[#0a6bb1]">
+                <Button className="bg-[#0e92f1] h-9 hover:bg-[#0a6bb1] ">
                   Follow
                 </Button>
               </div>
-              <div className="flex gap-8 justify-center">
+              <div className="flex gap-8 justify-center max-[375px]:hidden">
                 <div className="flex flex-col items-center">
                   <div>9</div>
                   <div className="text-[#aeaeae]">Posts</div>
@@ -63,10 +63,28 @@ const Profile = () => {
         ) : (
           ""
         )}
-        <div className="px-4 mt-4">{`${
-          user.bio.length ? user.bio : "No Bio"
+        <div className="mt-4 px-1">{`${
+          user?.bio?.length ? user?.bio : "No Bio"
         }`}</div>
-        <div className="flex flex-col overflow-auto border-t ">posts</div>
+        <div className="max-[375px]:flex hidden">
+          <div className="flex flex-row w-full justify-evenly px-1">
+            <div className="flex flex-col items-center">
+              <div>9</div>
+              <div className="text-[#aeaeae]">Posts</div>
+            </div>
+            <div className="flex flex-col items-center">
+              <div>9</div>
+              <div className="text-[#aeaeae]">Followers</div>
+            </div>
+            <div className="flex flex-col items-center">
+              <div>9</div>
+              <div className="text-[#aeaeae]">Following</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col w-[600px] max-w-full bg-red-500 overflow-auto border-t border">
+        posts
       </div>
       <Sidebar />
       <BottomNavBar />
